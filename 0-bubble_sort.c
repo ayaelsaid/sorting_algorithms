77b5e9517322d4be744f39949b9ccf5804ab09fe
ayaelsaid <ayae7675@gmail.com>
2024-01-22 13:54:07 -0800

    0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+/**
+ * bubble_sort - compare every number with all
+ * @array: array of numbers
+ * @size: size of array
+ * Return: ...
+*/
+void bubble_sort(int *array, size_t size)
+{
+	int j, i;
+	int temp;
+
+
