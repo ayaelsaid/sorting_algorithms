843f6b23f1ca98c0372b145a9ae7b2cb0c2427ac
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-01-24 04:14:58 +0200

    Create 3-O

--- sort.h
+++ sort.h
@@ -4,6 +4,9 @@
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
+int partition(int *arr, int low, int high, size_t size);
+void _sort(int *array, int lo, int hi, size_t size);
+void quick_sort(int *array, size_t size);
 
 /**
  * struct listint_s - Doubly linked list node
